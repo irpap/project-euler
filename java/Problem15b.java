abd3cc57978a2e0110e23a351354f80fb559cd8f
Irene Papakonstantinou <eirhnh@gmail.com>
2012-06-16 20:53:01 +0100

    Oh, it turns out that the number of shortest paths in this graph is equivalent to the central bionomial coefficient of the width

---
+++ java/Problem15b.java
@@ -0,0 +1,29 @@
+public class Problem15b {
+    public static int SIZE = 21;
+
+    public static void main(String[] args) {
+        long[][] triangle = pascalTriangle(SIZE * 2);
+        long paths = centralBionomialCoeff(triangle, SIZE);
+        System.out.println(paths);
+    }
+
+    public static long centralBionomialCoeff(long[][] triangle, int i) {
+        return triangle[(i - 1) * 2][i - 1];
+
+    }
+
+    public static long[][] pascalTriangle(int n) {
+        long[][] triangle = new long[n][];
+        for (int i = 0; i < n; i++) {
+            triangle[i] = new long[i + 1];
+            triangle[i][0] = 1;
+            triangle[i][i] = 1;
+        }
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 1; j < triangle[i].length - 1; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+        return triangle;
+    }
+}
