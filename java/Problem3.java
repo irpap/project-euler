c8873131f2032f9c10de19a6f327ef791344fcd3
Irene Papakonstantinou <eirhnh@gmail.com>
2012-06-19 13:58:09 +0100

    factoring out a helper class

--- java/Problem3.java
+++ java/Problem3.java
@@ -1,25 +1,18 @@
+import util.EulerHelper;
+
 public class Problem3 {
 
-    private final long number = 600851475143L;
-    private final int sqrtOfNumber = (int) Math.sqrt(number);
+    public static void main(String[] args) {
+        System.out.println(largestPrimeFactor());
+    }
 
-    public int largestPrimeFactor() {
-        boolean[] isPrime = primes();
+    public static int largestPrimeFactor() {
+        long number = 600851475143L;
+        boolean[] isPrime = EulerHelper.primesUpTo(number);
         for (int i = isPrime.length - 1; i > 0; i--) {
             if (isPrime[i] && number % i == 0) return i;
         }
         return -1;
-    }
-
-    public boolean[] primes() {
-        boolean[] isPrime = new boolean[sqrtOfNumber];
-        for (int i = 0; i < isPrime.length; i++) isPrime[i] = true;
-        for (int j = 2; j < isPrime.length; j++) {
-            int prime = j;
-            if (!isPrime[prime]) continue;
-            for (int i = prime * 2; i < isPrime.length; i = i + prime) isPrime[i] = false;
-        }
-        return isPrime;
     }
 
 
