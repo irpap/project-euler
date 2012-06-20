79d13ca42cf90aefb91cf0797df12bc348300276
Irene Papakonstantinou <eirhnh@gmail.com>
2012-06-20 14:36:21 +0100

    still in progress

--- java/Problem24.java
+++ java/Problem24.java
@@ -29,31 +29,35 @@ public class Problem24 {
 
         int ms = ms(n); //the least significant digit which is followed by a larger even less significant digit
         int ls = ls(n, ms); // the immediately bigger digit with less significance than ms.
-        if (ls == -1) return null; // no more permutations
+        if (ls == ms) return null; // no more permutations
 
         int tmp = n[ls];
         n[ls] = n[ms];
         n[ms] = tmp;
 
-        Arrays.sort(n, ms + 1, n.length - 1);
+//        Arrays.sort(n, ms + 1, n.length - 1);
         return n;
 
     }
 
-    private static int ls(int[] n, int ms) {
-        int minLarger = ms;
-        for (int i = ms; i < n.length - 1; i++) {
-            if (n[i] > n[ms] && n[i] < minLarger) minLarger = n[i];
-        }
-        return minLarger;
-    }
-
     private static int ms(int[] n) {
         for (int i = n.length - 2; i > 0; i--) {
-            for (int j = i; j < n.length - 1; j++) {
-                if (j > i) return i;
+            for (int j = i; j < n.length; j++) {
+                if (n[j] > n[i]) return i;
             }
         }
         return -1;
     }
+
+    private static int ls(int[] n, int ms) {
+        int minLarger = ms;
+        for (int i = ms + 1; i < n.length; i++) {
+            if (n[i] > n[ms]) {
+                if (minLarger == ms) minLarger = i;
+                else minLarger = n[i] <= n[minLarger] ? i : minLarger;
+            }
+        }
+        return minLarger;
+    }
+
 }
