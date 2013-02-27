68ba5620854d6b51148b6be1bd6bdbb7ffc0eaa1
Irene Papakonstantinou <eirhnh@gmail.com>
2013-02-27 22:26:32 +0000

    Trying to make it more functional

--- scala/problem19.scala
+++ scala/problem19.scala
@@ -1,26 +1,25 @@
  object Problem19 {
     def main(args: Array[String]) {
-        var count = 0
-        for (y <- (1901 to 2000); m <- (1 to 12)) {
-         if (isMonday(1,m,y)) count+=1
-    }
-    println(count)
+        val mondays:Seq[Unit] = (for (y <- (1901 to 2000); m <- (1 to 12); if (isMonday(1,m,y))) yield ())
+        println(mondays.length)
    }
-   def isMonday(d:Int, m:Int, y:Int):Boolean = {
-       val r = rank(d,m,y)
-       r % 7 == 0
-   }
+
+   def isMonday(d:Int, m:Int, y:Int):Boolean = rank(d,m,y) % 7 == 0
+   
    def rank(d:Int, m:Int, y:Int):Int = {
        //days since 1-Jan-1900
-       d + (0 until m).map(m => daysInMonth(m,y)).sum + (1900 until y).map(daysInYear).sum 
+       d + (0 until m).map(daysInMonth(_,y)).sum + (1900 until y).map(daysInYear).sum 
    }
-   def daysInYear(y:Int) = if (isLeap(y)) 366 else 365
-   def isLeap(y:Int):Boolean = {
-       y % 4 == 0 && (y%100 != 0 || y % 400 == 0) 
-   }
+   def isLeap(y:Int):Boolean = y % 4 == 0 && (y%100 != 0 || y % 400 == 0) 
+   
    def daysInMonth(y:Int, m:Int):Int = {
-       if (List(4,6,9) contains m) 30
-           else if (m==2) {if (isLeap(y)) 29 else 28}
-           else 31
+       (m,y) match {
+        case (2, y) if isLeap(y) => 29
+        case (2, _ ) => 28
+        case (m, _) if List(4,6,9,11) contains m => 30
+        case (_, _) => 31
+       }
    }
+
+   def daysInYear(y:Int) = if (isLeap(y)) 366 else 365
 }
